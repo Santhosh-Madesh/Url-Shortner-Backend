0832c48c1d01e414ac88e1bca848aa586e90881a
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-05-27 01:00:46 +0530

    replace with the correct origin

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "https://replit.com/@santoshmadesh05/Playful-Website-Builder",
+        origin: "https://playful-website-builder--santoshmadesh05.replit.app/",
         methods: ["GET","POST","PUT", "DELETE"],
         credentials:true
     })
